0b3024ecd89be7e9238b8eccd6039edb5f564bbe
postmodernist1488 <elitic.pantheism@gmail.com>
2024-01-21 11:13:57 +0300

    Add chat

--- main.go
+++ main.go
@@ -1,33 +1,35 @@
 package main
 
 import (
+	"bytes"
 	_ "embed"
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
-    "strings"
-    "fmt"
-    "io"
-    "postmodernist1848.ru/githublines"
-    "encoding/json"
-    "bytes"
+	"postmodernist1848.ru/githublines"
+	"strings"
 )
 
 //go:embed index.html.tmpl
 var indexTemplateString string
 var indexTemplate = template.Must(template.New("index").Parse(indexTemplateString))
+
 //go:embed log.html.tmpl
 var logTemplateString string
 var logTemplate = template.Must(template.New("log").Parse(logTemplateString))
 var errorContents = []byte("<h1>404: this page does not exist</h1>")
 
 var pathToFile = map[string]string{
-	"/":      "index.html",
-	"/manifesto":  "manifesto.html",
-	"/funi":  "funi.html",
-	"/game":  "game.html",
-	"/links": "links.html",
+	"/":          "index.html",
+	"/manifesto": "manifesto.html",
+	"/funi":      "funi.html",
+	"/game":      "game.html",
+	"/chat":      "chat.html",
+	"/links":     "links.html",
 }
 
 func getContents(path string) []byte {
@@ -57,39 +59,39 @@ func serveRoot(w http.ResponseWriter, r *http.Request) {
 
 /* fetch the pastebin blog */
 func getRawLogHTML() ([]byte, error) {
-    const url = "https://pastebin.com/raw/vb43aqyz";
-    resp, err := http.Get(url);
-    if (err != nil) {
-        return nil, err;
-    }
-    defer resp.Body.Close()
-    text, err := io.ReadAll(resp.Body)
-    if (err != nil) {
-        return nil, err;
-    }
-    return text, nil
+	const url = "https://pastebin.com/raw/vb43aqyz"
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	text, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return text, nil
 }
 
 /* insert into the log template */
 func processRawLogHTML(rawHTML []byte) ([]byte, error) {
-    var tpl bytes.Buffer
+	var tpl bytes.Buffer
 	data := map[string]interface{}{
 		"contents": template.HTML(rawHTML),
 	}
-    if err := logTemplate.Execute(&tpl, data); err != nil {
-        return nil, err
-    }
-    return tpl.Bytes(), nil
+	if err := logTemplate.Execute(&tpl, data); err != nil {
+		return nil, err
+	}
+	return tpl.Bytes(), nil
 }
 
 func serveLog(w http.ResponseWriter, r *http.Request) {
-    rawLogHTML, err := getRawLogHTML()
-    if (err != nil) {
-        log.Println(err)
-        w.Write(errorContents)
-    }
+	rawLogHTML, err := getRawLogHTML()
+	if err != nil {
+		log.Println(err)
+		w.Write(errorContents)
+	}
 
-    logHTML, err := processRawLogHTML(rawLogHTML);
+	logHTML, err := processRawLogHTML(rawLogHTML)
 
 	data := map[string]interface{}{
 		"contents": template.HTML(logHTML),
@@ -107,64 +109,97 @@ func serveStaticFile(w http.ResponseWriter, r *http.Request) {
 }
 
 func countLinesRepoResponse(w http.ResponseWriter, r *http.Request) {
-    //TODO: use atomic counting to limit number of simultaneous requests
+	//TODO: use atomic counting to limit number of simultaneous requests
 
-    username := strings.TrimPrefix(r.URL.Path, "/countlines/")
-    log.Printf("Handling request. Username: %v", username)
-    url := fmt.Sprintf("https://api.github.com/users/%v/repos", username)
-    resp, err := http.Get(url)
-    if err != nil {
-        log.Println("GithubLines: HTTP GET error for", username, err)
-        io.WriteString(w, "Failure getting data from Github")
-        return
-    }
-    defer resp.Body.Close()
-    if resp.StatusCode != http.StatusOK {
-        bodyBytes, err := io.ReadAll(resp.Body)
-        if (err != nil) {
-            log.Println("GithubLines: HTTP resp not OK for", username, resp.StatusCode)
-        } else {
-            log.Println("GithubLines: HTTP resp not OK for", username, string(bodyBytes))
-        }
-        io.WriteString(w, "Failure getting data from Github")
-        return
-    }
-    var result []githublines.Repo
-    if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-        log.Println("GithubLines: Decoding error for", username, err)
-        io.WriteString(w, "Failure decoding data from Github")
-        return
-    }
-    c := make(chan githublines.RepoData)
-    for _, repo := range result {
-        go githublines.CountLinesRepo(repo, c)
-    }
-    io.WriteString(w, "<ul>")
-    totalCount := 0;
-    for range result {
-        repo := <-c
-        io.WriteString(w, fmt.Sprintf("<li>%v: %v lines</li>", repo.Name, repo.LineCount))
-        totalCount += repo.LineCount
-    }
-    io.WriteString(w, "<ul>")
-    io.WriteString(w, fmt.Sprintf("Total: %v lines", totalCount))
+	username := strings.TrimPrefix(r.URL.Path, "/countlines/")
+	log.Printf("Handling request. Username: %v", username)
+	url := fmt.Sprintf("https://api.github.com/users/%v/repos", username)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println("GithubLines: HTTP GET error for", username, err)
+		io.WriteString(w, "Failure getting data from Github")
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Println("GithubLines: HTTP resp not OK for", username, resp.StatusCode)
+		} else {
+			log.Println("GithubLines: HTTP resp not OK for", username, string(bodyBytes))
+		}
+		io.WriteString(w, "Failure getting data from Github")
+		return
+	}
+	var result []githublines.Repo
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		log.Println("GithubLines: Decoding error for", username, err)
+		io.WriteString(w, "Failure decoding data from Github")
+		return
+	}
+	c := make(chan githublines.RepoData)
+	for _, repo := range result {
+		go githublines.CountLinesRepo(repo, c)
+	}
+	io.WriteString(w, "<ul>")
+	totalCount := 0
+	for range result {
+		repo := <-c
+		io.WriteString(w, fmt.Sprintf("<li>%v: %v lines</li>", repo.Name, repo.LineCount))
+		totalCount += repo.LineCount
+	}
+	io.WriteString(w, "<ul>")
+	io.WriteString(w, fmt.Sprintf("Total: %v lines", totalCount))
+}
+
+type ChatMessage struct {
+	Author string `json:"author"`
+	Text   string `json:"text"`
+}
+
+var messages []ChatMessage
+
+func serveChatMessages(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<ul style=\"list-style: none\">"))
+	for _, msg := range messages {
+		w.Write([]byte("<li>"))
+		w.Write([]byte(msg.Author))
+		w.Write([]byte(": "))
+		w.Write([]byte(msg.Text))
+		w.Write([]byte("</li>"))
+	}
+	w.Write([]byte("</ul>"))
+}
+func chatSendHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var msg ChatMessage
+	err := decoder.Decode(&msg)
+	if err != nil {
+		log.Println("failed to parse message: ", err)
+		w.WriteHeader(400)
+		return
+	}
+	log.Println("Received message: ", msg)
+	messages = append(messages, msg)
 }
 
 func main() {
-    http_port := "80"
-    https_port := "443"
+	http_port := "80"
+	https_port := "443"
 
 	http.HandleFunc("/", serveRoot)
 	http.HandleFunc("/log", serveLog)
+	http.HandleFunc("/chat/messages", serveChatMessages)
+	http.HandleFunc("/chat/send", chatSendHandler)
 	http.HandleFunc("/static/", serveStaticFile)
 	http.HandleFunc("/assets/", serveStaticFile)
-    http.HandleFunc("/countlines/", countLinesRepoResponse)
+	http.HandleFunc("/countlines/", countLinesRepoResponse)
 
 	log.Println("listening for http on", http_port)
 	go func() {
-        log.Fatal(http.ListenAndServe(":"+http_port, nil))
-    }()
+		log.Fatal(http.ListenAndServe(":"+http_port, nil))
+	}()
 
 	log.Println("listening for https on", https_port)
-    log.Fatal(http.ListenAndServeTLS(":"+https_port, "server.crt", "server.key", nil))
+	log.Fatal(http.ListenAndServeTLS(":"+https_port, "server.crt", "server.key", nil))
 }
