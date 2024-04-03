45b714f5a4cf1b93c07353aa4c6c6d879ebf34cb
Vladimir Ivanov <elitic.pantheism@gmail.com>
2024-04-03 19:16:39 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@ set -e
 
 docker build -t 'postmodernist1848.ru-server:latest' .
 
-if [ "$(docker ps -q -f name=server-instance)" ]; then
-    docker stop server-instance
+if [ "$(docker ps -q -f name="server-instance")" ]; then
+    docker stop "server-instance"
 fi
-docker run --name server-instance --detach -p 80:80 -p 443:443 --rm -v /root/database.db:/database.db postmodernist1848.ru-server:latest
-docker logs -f server-instance &> server.log &
+docker run --name "server-instance" --detach -p 80:80 -p 443:443 --rm -v /root/database.db:/database.db postmodernist1848.ru-server:latest
+docker logs -f "server-instance" &> server.log &
 docker image prune -f
