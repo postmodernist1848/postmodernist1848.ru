b72483bf1fd35e4d166c775d18d3053c06e2c882
Vladimir Ivanov <elitic.pantheism@gmail.com>
2024-04-17 17:00:00 +0300

    [skip ci] add log file as Docker volume

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ if [ "$(docker ps -q -f name="server-instance")" ]; then
     docker stop "server-instance"
     docker rm "server-instance"
 fi
-docker run --name "server-instance" --detach -p 80:80 -p 443:443 -v /root/database.db:/database.db postmodernist1848.ru-server:latest
+docker run --name "server-instance" --detach -p 80:80 -p 443:443 -v /root/database.db:/database.db -v /root/log.html:/log.html postmodernist1848.ru-server:latest
 docker logs -f "server-instance" &> server.log &
 docker image prune -f
