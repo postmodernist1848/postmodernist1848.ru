05383ffc5e35b6215b1248beb9b2b07d775e01c7
Vladimir Ivanov <elitic.pantheism@gmail.com>
2024-05-04 20:33:53 +0300

    [no ci] migrate to let's encrypt SSL certificate

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,14 @@ set -e
 
 docker build -t 'postmodernist1848.ru-server:latest' .
 
-if [ "$(docker ps -q -f name="server-instance")" ]; then
-    docker stop "server-instance"
+if [ "$(docker ps -aq -f name="server-instance")" ]; then
     docker rm "server-instance"
 fi
-docker run --name "server-instance" --detach -p 80:80 -p 443:443 -v /root/database.db:/database.db -v /root/log.html:/log.html postmodernist1848.ru-server:latest
+docker run --name "server-instance" --detach -p 80:80 -p 443:443 \
+    -v /root/database.db:/database.db \
+    -v /root/log.html:/log.html \
+    -v /etc/letsencrypt/live/www.postmodernist1848.ru/fullchain.pem:/server.crt \
+    -v /etc/letsencrypt/live/www.postmodernist1848.ru/privkey.pem:/server.key \
+    postmodernist1848.ru-server:latest
 docker logs -f "server-instance" &> server.log &
 docker image prune -f
