9cb5931ddda4cbc6173599fcdd8930c95664a0ad
postmodernist1488 <elitic.pantheism@gmail.com>
2024-05-04 20:48:33 +0300

    stop server-instance container before rm-ing

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ set -e
 docker build -t 'postmodernist1848.ru-server:latest' .
 
 if [ "$(docker ps -aq -f name="server-instance")" ]; then
+    docker stop "server-instance"
     docker rm "server-instance"
 fi
 docker run --name "server-instance" --detach -p 80:80 -p 443:443 \
