13c8687c0f90d296ae4c3b3a02f5757df0d0af98
Vladimir Ivanov <elitic.pantheism@gmail.com>
2025-03-27 00:54:14 +0300

    add log integration tests

--- cmd/server/integration_test.go
+++ cmd/server/integration_test.go
@@ -5,10 +5,14 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"html/template"
 	"io"
 	"math/rand"
 	"net/http"
+	"os"
+	"postmodernist1848.ru/domain"
 	"postmodernist1848.ru/internal/server"
+	"slices"
 	"testing"
 )
 
@@ -48,19 +52,37 @@ func httpGET(t *testing.T, path string) []byte {
 	return bytes
 }
 
-func httpPOST(t *testing.T, path string, body any) []byte {
+func httpRequest(t *testing.T, path string, method string, body any, username string, password string) []byte {
 	bodyJSON, err := json.Marshal(body)
 	if err != nil {
 		t.Fatal(err)
 	}
-	resp, err := http.Post("http://localhost"+testServerAddr+path, "application/json", bytes.NewReader(bodyJSON))
+
+	req, err := http.NewRequest(
+		method,
+		"http://localhost"+testServerAddr+path,
+		bytes.NewReader(bodyJSON),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	if username != "" {
+		req.SetBasicAuth(username, password)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer resp.Body.Close()
+
 	if resp.StatusCode != http.StatusOK {
-		t.Fatalf("%s: got %d, want %d", path, resp.StatusCode, http.StatusOK)
+		t.Fatalf("%s %s: got %d, want %d",
+			method, path, resp.StatusCode, http.StatusOK)
 	}
+
+	// Read and return response body
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		t.Fatal(err)
@@ -107,14 +129,45 @@ func TestAllOk(t *testing.T) {
 	t.Run("chat", func(t *testing.T) {
 		author := RandStringBytes(rand.Int()%10 + 4)
 		text := RandStringBytes(rand.Int()%100 + 10)
-		httpPOST(t, "/api/message", map[string]interface{}{
+		httpRequest(t, "/api/message", http.MethodPost, map[string]interface{}{
 			"author": author,
 			"text":   text,
-		})
+		}, "", "")
 		res := httpGET(t, "/api/message")
 		if !bytes.HasSuffix(res, []byte(fmt.Sprintf("<li>%s: %s</li></ul>", author, text))) {
 			t.Log("expected new message, but got")
 			t.Log(string(res))
+			t.FailNow()
+		}
+	})
+
+	t.Run("log", func(t *testing.T) {
+		const testPassword = "password123"
+		if err := os.WriteFile("api_token", []byte(testPassword), 0600); err != nil {
+			t.Fatal(err)
+		}
+		t.Cleanup(func() {
+			if err := os.Remove("api_token"); err != nil {
+				t.Error(err)
+			}
+		})
+		var logs []domain.Log
+		for i := range 20 {
+			text := RandStringBytes(rand.Int()%100 + 10)
+			logs = append(logs, domain.Log{fmt.Sprintf("%d.03.42", i+1), template.HTML(text)})
+		}
+		httpRequest(t, "/api/log", http.MethodPut, logs, "postmodernist1848", testPassword)
+		res := httpGET(t, "/api/log")
+		var resLogs []domain.Log
+		if err := json.Unmarshal(res, &resLogs); err != nil {
+			t.Fatal(err)
+		}
+		if !slices.Equal(logs, resLogs) {
+			t.Log("logs not equal")
+			t.Log("uploaded:")
+			t.Log(logs)
+			t.Log("received:")
+			t.Log(resLogs)
 			t.FailNow()
 		}
 	})
