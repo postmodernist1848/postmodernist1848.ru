1476f6dc491af20d3b5c44bcd9666310e6a58e4e
Vladimir Ivanov <elitic.pantheism@gmail.com>
2025-03-27 00:54:03 +0300

    use go 1.22 mux for log api

--- internal/server/server.go
+++ internal/server/server.go
@@ -122,53 +122,50 @@ func serveStaticFile(w http.ResponseWriter, r *http.Request) {
 	serveFile(w, r, strings.TrimPrefix(r.URL.Path, "/"))
 }
 
-func getPostLogHandler(w http.ResponseWriter, r *http.Request) {
-	//TODO: rewrite using go 1.22 mux
-	if r.Method == "GET" {
-		logs, err := sqlite.GetLogs()
-		if err != nil {
-			log.Println("Could not get logs:", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		JSON, err := json.Marshal(logs)
-		if err != nil {
-			log.Println("Could not marshal logs JSON:", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		_, _ = w.Write(JSON)
-	} else if r.Method == "PUT" {
-		username, password, ok := r.BasicAuth()
-		if !ok {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		token, err := os.ReadFile("api_token")
-		if err != nil {
-			log.Println("Could not read token file:", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if username != "postmodernist1848" || string(token) != password {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		var logs []domain.Log
-		err = json.NewDecoder(r.Body).Decode(&logs)
-		if err != nil {
-			log.Println("Could not unmarshal logs JSON:", err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		log.Println("Rewriting logs...")
-		err = sqlite.RewriteLogs(logs)
-		if err != nil {
-			log.Println("Could not rewrite logs:", err)
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
+func getLogHandler(w http.ResponseWriter, r *http.Request) {
+	logs, err := sqlite.GetLogs()
+	if err != nil {
+		log.Println("Could not get logs:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	JSON, err := json.Marshal(logs)
+	if err != nil {
+		log.Println("Could not marshal logs JSON:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, _ = w.Write(JSON)
+}
+
+func putLogHandler(w http.ResponseWriter, r *http.Request) {
+	username, password, ok := r.BasicAuth()
+	if !ok {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	token, err := os.ReadFile("api_token")
+	if err != nil {
+		log.Println("Could not read token file:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if username != "postmodernist1848" || string(token) != password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	var logs []domain.Log
+	err = json.NewDecoder(r.Body).Decode(&logs)
+	if err != nil {
+		log.Println("Could not unmarshal logs JSON:", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	log.Println("Rewriting logs...")
+	err = sqlite.RewriteLogs(logs)
+	if err != nil {
+		log.Println("Could not rewrite logs:", err)
+		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
 
@@ -239,7 +236,8 @@ func New(addr string) *http.Server {
 	mux.HandleFunc("POST /api/message", postChatMessageHandler)
 
 	mux.HandleFunc("/api/countlines/", githublines.CountlinesHandler)
-	mux.HandleFunc("/api/log", getPostLogHandler)
+	mux.HandleFunc("GET /api/log", getLogHandler)
+	mux.HandleFunc("PUT /api/log", putLogHandler)
 
 	return &http.Server{Addr: addr, Handler: mux}
 }
